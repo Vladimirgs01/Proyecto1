fae1bd14a5df56862b5d7ad57f1100525b81f191
Vladimirgs01 <vladiguzman121@gmail.com>
2023-05-14 13:07:22 -0400

    Eliminando el namespace

--- Prioridades/Models/Registros_Prioridad.cs
+++ Prioridades/Models/Registros_Prioridad.cs
@@ -1,15 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace RegistroPrioridad.Models{
-    public class Registros_Prioridad{
+public class Registros_Prioridad{
 
-        [Key]
-        public int PrioridadId { get; set;}
+    [Key]
+    public int PrioridadId { get; set;}
 
-        [Required (ErrorMessage ="La descripcion es obligatorio")]
-        public string Descripcion { get; set; } = string.Empty;
+    [Required (ErrorMessage ="La descripcion es obligatorio")]
+    public string Descripcion { get; set; } = string.Empty;
 
-        [Required (ErrorMessage ="Los dia son necesarios")]
-        public DateTime DiasCompromiso { get; set; } = DateTime.Today;
-    }
+    [Required (ErrorMessage ="Los dia son necesarios")]
+    public DateTime DiasCompromiso { get; set; } = DateTime.Today;
 }
